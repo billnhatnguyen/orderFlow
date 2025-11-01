3d06f250c56092e37649f95eb849eb209e2ef0da
krishivpan <103297164+krishivpan@users.noreply.github.com>
2025-11-01 18:34:38 -0400

    Added the header component, fonts and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Navbar from "./components/Navbar.jsx"
+import Header from "./components/Header.jsx"
 import './index.css'
 
 export default function App() {
   return (
-    <Navbar/>
+    <>
+      <Navbar/>
+      <Header/>
+    </>  
   )
 }

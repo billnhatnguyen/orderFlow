9fe4c7d78765a0c42e563def7b49ca74335f23b5
krishivpan <103297164+krishivpan@users.noreply.github.com>
2025-11-01 19:30:20 -0400

    Added the main content component, entries and styled them, still a few design fixes need to be made

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar.jsx"
 import Header from "./components/Header.jsx"
+import SalesQueue from "./components/SalesQueue.jsx"
 import './index.css'
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <>
       <Navbar/>
       <Header/>
+      <SalesQueue/>
     </>  
   )
 }
--- src/components/SalesQueue.jsx
+++ src/components/SalesQueue.jsx
@@ -1,5 +1,49 @@
 export default function SalesQueue() {
     return (
-        <p>Temp</p>
+        <main>
+            <div className="info-text">
+                <h2>Lettuce Begin!</h2>
+                <p>Check out your sales calls queue below!</p>
+            </div>
+
+            <div className="entries">
+                <div className="entry">
+                    <div className="entry-number">
+                        <span>Entry</span>
+                        <span>#01</span>
+                    </div>
+
+                    <img src="src/images/food.png" className="food-item-img" alt="Plate of tomato basil pasta"/>
+
+                    <span>Tomato Basil Pasta</span>
+                    <img src="src/images/Button.png" className="arrow-btn" alt="arrow pointing to the downwards right direction"/>
+                </div>
+
+                <div className="entry">
+                    <div className="entry-number">
+                        <span>Entry</span>
+                        <span>#02</span>
+                    </div>
+
+                    <img src="src/images/apple.png" className="food-item-img" alt="An apple"/>
+
+                    <span>Apple Citrus Salad</span>
+                    <img src="src/images/Button.png" className="arrow-btn" alt="arrow pointing to the downwards right direction"/>
+                </div>
+
+                <div className="entry">
+                    <div className="entry-number">
+                        <span>Entry</span>
+                        <span>#03</span>
+                    </div>
+
+                    <img src="src/images/pumpkin.png" className="food-item-img" alt="Pumpkin"/>
+
+                    <span>Pumpkin Soup</span>
+                    <img src="src/images/Button.png" className="arrow-btn" alt="arrow pointing to the downwards right direction"/>
+                </div>
+            </div>
+
+        </main>
     )
 }
